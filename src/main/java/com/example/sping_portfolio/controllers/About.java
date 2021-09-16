409dfc8711e7ee81acb20c4dccb10af4301bbf37
Kevin Do <54718041+kevin-dough@users.noreply.github.com>
2021-09-16 10:23:01 -0700

    about page stuff

--- src/main/java/com/example/sping_portfolio/controllers/About.java
+++ src/main/java/com/example/sping_portfolio/controllers/About.java
@@ -1,25 +1,39 @@
 package com.example.sping_portfolio.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class About {
+    private String name;
+    private String donut;
 
-    @GetMapping("/about")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String about(Model model) {
+    public About()
+    {
 
-        String[] name = new String[] { "Kevin", "Ak", "Navodit", "Eshaan" };
-
-        model.addAttribute("name", name);
-
-        String[] donut = new String[] { "Glazed", "Example", "Example", "Example" };
-
-        model.addAttribute("donut", donut);
-
-        return "about"; // returns HTML VIEW (greeting)
     }
-}
 
+    public About(String name, String donut) {
+        //create an attribute
+        this.name = name;
+        this.donut = donut;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDonut() {
+        return donut;
+    }
+
+    public void setDonut(String donut) {
+        this.donut = donut;
+    }
+
+    @Override
+    public String toString() {
+        return "About [name=" + name + ", donut=" + donut + "]";
+    }
+
+}
---
+++ src/main/java/com/example/sping_portfolio/controllers/AboutController.java
@@ -0,0 +1,46 @@
+package com.example.sping_portfolio.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.sping_portfolio.controllers.About;
+
+@Controller
+
+public class AboutController {
+
+    private List<About> listAbout;
+
+    @PostConstruct
+    private void loadData()
+    {
+        //create an object by creating an instance in the class
+        About person1=new About("Kevin","Cherry Glaze");
+        About person2=new About("Ak","Sugar Donut");
+        About person3=new About("Navodit","Original Glaze");
+        About person4=new About("Eshaan","Vanilla Sprinkle");
+
+        listAbout=new ArrayList<>();
+
+        listAbout.add(person1);
+        listAbout.add(person2);
+        listAbout.add(person3);
+        listAbout.add(person4);
+
+    }
+
+    @GetMapping("/about")
+    public String showAbout(Model model)
+    {
+
+        model.addAttribute("about", listAbout);
+        return "about";
+    }
+}
