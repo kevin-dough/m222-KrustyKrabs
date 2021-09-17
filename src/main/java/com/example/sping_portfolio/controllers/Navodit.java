0bb9768856bca83dc7786c4c6ae3c5e63cc4c1b8
Navodit1603 <navoditmaheshwari@gmail.com>
2021-09-17 10:36:21 -0700

    I AM NOT A KRUSTY KRAB

--- src/main/java/com/example/sping_portfolio/controllers/Navodit.java
+++ src/main/java/com/example/sping_portfolio/controllers/Navodit.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Navodit {
 
     @GetMapping("/navodit")  // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Patrick") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
         return "navodit"; // returns HTML VIEW (greeting)
