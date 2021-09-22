9a294ffadc1f35f570c9bbc97345aec1dd55b22a
Kevin Do <54718041+kevin-dough@users.noreply.github.com>
2021-09-22 10:19:23 -0700

    grayscale page

---
+++ src/main/java/com/example/sping_portfolio/controllers/Grayscale.java
@@ -0,0 +1,15 @@
+package com.example.sping_portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Grayscale {
+
+    @GetMapping("/grayscale")
+    public String grayscale() {
+        return "grayscale";
+    }
+}
