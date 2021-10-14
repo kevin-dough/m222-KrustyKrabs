c2311569294b782c900992797f92a0c6d70996b1
Navodit1603 <navoditmaheshwari@gmail.com>
2021-10-14 10:26:27 -0700

    maven dependency

--- src/main/java/com/example/sping_portfolio/SQL/Donut.java
+++ src/main/java/com/example/sping_portfolio/SQL/Donut.java
@@ -1,4 +1,18 @@
 package com.example.sping_portfolio.SQL;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Donut {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
 }
