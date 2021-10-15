f84133d7b320bc1d27f75d38fb3148cf08e9b993
Navodit1603 <navoditmaheshwari@gmail.com>
2021-10-15 09:48:34 -0700

    donut db

--- src/main/java/com/example/sping_portfolio/SQL/Donut.java
+++ src/main/java/com/example/sping_portfolio/SQL/Donut.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 @NoArgsConstructor
 @Getter
@@ -14,5 +15,21 @@ public class Donut {
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @NonNull
+    @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
+    private String name;
+
+    @NonNull
+    @Size(min = 2, max = 30, message = "Store name(2 to 30 chars)")
+    private String store;
+
+
+
+    public Donut(String name, String store, Integer rating){
+        this.name = name;
+        this.store = store;
+        this.rating = rating;
+    }
 
 }
